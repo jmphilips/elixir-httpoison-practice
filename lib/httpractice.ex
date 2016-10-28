cdfbc59e061ce7e79534903ce721ccfcbb311cdd
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-27 23:52:34 -0500

    Added a function that counts the number of times each word is used

--- lib/httpractice.ex
+++ lib/httpractice.ex
@@ -14,8 +14,13 @@ defmodule Httpractice do
         String.strip(string)
         |> String.downcase
         |> String.split(" ")
+        |> reduce
     end
 
-
+    defp reduce(word_list) do
+        Enum.reduce word_list, %{}, fn (i, acc) -> 
+             Dict.update acc, i, 1, &(&1 + 1)
+        end
+    end
 
 end
